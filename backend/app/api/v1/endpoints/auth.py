14bc928761dc5584a94a9efe955f215ddfcd586d
Athos Danilo <athosdanilomarques@gmail.com>
2026-02-23 14:40:06 -0300

    Ajuste

--- backend/app/api/v1/endpoints/auth.py
+++ backend/app/api/v1/endpoints/auth.py
@@ -1,7 +1,8 @@
-# backend/app/api/v1/endpoints/auth.py
 from fastapi import APIRouter
 from fastapi.responses import RedirectResponse
 import urllib.parse
+import requests
+import base64
 from app.core.config import settings
 
 router = APIRouter()
@@ -31,4 +32,25 @@ def login_spotify():
 
 @router.get("/callback")
 def callback_spotify(code: str):
-    return {"message": "Login feito com sucesso! Recebemos o código.", "code": code}
+    # 1. Preparar a autenticação (Client ID + Secret codificados em Base64)
+    auth_string = f"{settings.SPOTIFY_CLIENT_ID}:{settings.SPOTIFY_CLIENT_SECRET}"
+    auth_bytes = auth_string.encode("utf-8")
+    auth_base64 = str(base64.b64encode(auth_bytes), "utf-8")
+
+    # 2. Configurar o pedido (Request) para pegar o Token
+    url = "https://accounts.spotify.com/api/token"
+    headers = {
+        "Authorization": "Basic " + auth_base64,
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+    data = {
+        "grant_type": "authorization_code",
+        "code": code,
+        "redirect_uri": settings.SPOTIFY_REDIRECT_URI
+    }
+
+    # 3. Fazer a troca (POST)
+    response = requests.post(url, headers=headers, data=data)
+    
+    # 4. Retornar o resultado (O Access Token real!)
+    return response.json()
