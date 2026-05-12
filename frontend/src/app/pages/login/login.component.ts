9519ff3a80cf047000d1edeccd306670750ae914
Athos Danilo <athosdanilomarques@gmail.com>
2026-05-12 15:06:47 -0300

    Base da Tela de Login

--- frontend/src/app/pages/login/login.component.ts
+++ frontend/src/app/pages/login/login.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Router } from '@angular/router'; // 🚨 Router adicionado
+import { FormsModule } from '@angular/forms'; // 🚨 NOVO: Para capturar os inputs
 import { LogoComponent } from '../../components/logo.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, RouterModule, LogoComponent],
+  imports: [CommonModule, RouterModule, FormsModule, LogoComponent], // 🚨 FormsModule adicionado
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
@@ -14,11 +15,41 @@ export class LoginComponent implements OnInit {
   
   modoEscuro = true;
 
+  // 🚨 Variáveis do Formulário
+  email = '';
+  senha = '';
+  carregando = false;
+  mensagemErro = '';
+
+  constructor(private router: Router) {}
+
   ngOnInit() {
-    // 🚨 O Login pergunta ao navegador: "Qual era o tema lá na Home?"
     const temaSalvo = localStorage.getItem('tunify_tema');
     if (temaSalvo === 'claro') {
       this.modoEscuro = false;
     }
+  }
+
+  // 🚨 A Função do Botão
+  entrarNoSistema() {
+    // 1. Limpa erros antigos e trava o botão
+    this.mensagemErro = '';
+    
+    if (!this.email || !this.senha) {
+      this.mensagemErro = 'Preencha todos os campos, boy!';
+      return;
+    }
+
+    this.carregando = true;
+
+    // 2. Aqui a gente vai chamar o seu AuthService depois!
+    console.log('Tentando logar com:', this.email, this.senha);
+    
+    // Simulação rápida só pra gente ver o botão funcionando (depois trocamos pela API real)
+    setTimeout(() => {
+      this.carregando = false;
+      // this.router.navigate(['/dashboard']);
+      this.mensagemErro = 'A API do backend ainda não está conectada kkkk!';
+    }, 1500);
   }
 }
